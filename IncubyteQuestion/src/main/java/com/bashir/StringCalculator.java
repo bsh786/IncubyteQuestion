d1930f8bd920af25b1fb0b01ebe556744142467e
bsh786 <bashirdragon@gmail.com>
2021-07-31 04:22:34 +0530

    Task 3 completed and all test cases passed

--- IncubyteQuestion/src/main/java/com/bashir/StringCalculator.java
+++ IncubyteQuestion/src/main/java/com/bashir/StringCalculator.java
@@ -10,22 +10,26 @@ public class StringCalculator {
 			return 0;
 		}
 		
-		if(!numbers.contains(","))  // String consists single number 
+		if(!numbers.contains(",") && !(numbers.contains("\n")))  // String consists single number 
 		{
 			int number =  Integer.parseInt(numbers);
 			return number;
 		}
-		else {
+		else {  // String consists multiple numbers
 			
 			int sum = 0;
 			
-			String[] numbersArray = numbers.split(",");
+			numbers.trim();
+			String[] numbersArray = numbers.split(",|\\r?\\n");
 			for(String numberAsString:numbersArray)
 			{
+			   if(numberAsString.equals(""))
+			   {}
+			   else {
 			   int number = Integer.parseInt(numberAsString);
 			   sum+=number;   
+			   }
 			}
-			
 			return sum;
 		}
 		
