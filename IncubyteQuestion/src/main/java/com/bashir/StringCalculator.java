5c8c7c3a551ad9473a40241ba4d4ec4e4ed6788c
bsh786 <bashirdragon@gmail.com>
2021-07-31 03:44:59 +0530

    Added code for task 1 with all test cases passed

--- IncubyteQuestion/src/main/java/com/bashir/StringCalculator.java
+++ IncubyteQuestion/src/main/java/com/bashir/StringCalculator.java
@@ -5,7 +5,31 @@ public class StringCalculator {
 
 	public int add(String numbers)
 	{
-		return 0;
+		if(numbers.equals("")) // Returning 0 for empty string
+		{
+			return 0;
+		}
+		
+		if(!numbers.contains(","))  // String consists single number 
+		{
+			int number =  Integer.parseInt(numbers);
+			return number;
+		}
+		else {
+			
+			int sum = 0;
+			
+			String[] numbersArray = numbers.split(",");
+			for(String numberAsString:numbersArray)
+			{
+			   int number = Integer.parseInt(numberAsString);
+			   sum+=number;   
+			}
+			
+			return sum;
+		}
+		
+		
 	}
 	
 }
